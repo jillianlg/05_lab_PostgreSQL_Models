66c0300859fd8d335c2a9fc3fc954e06fac791bc
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-07 15:55:02 -0800

    add find route for Candy class

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Candy = require('./lib/models/Candy');
 
 
 Candy
-  .insert({ type: 'chocolate', description: 'milk chocolate, dark chocolate or white chocolate', url: 'https://url.com' })
+//   .insert({ type: 'chocolate', description: 'milk chocolate, dark chocolate or white chocolate', url: 'https://url.com' })
+  .find()  
   .then(console.log);
 
