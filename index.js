658d475a2e7f50da6dca46dbbbb8dacd389a860f
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-07 16:18:10 -0800

    adds DELETE route for Candy class and returns in console, confirm delete with Find

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const Candy = require('./lib/models/Candy');
 
 Candy
 //   .insert({ type: 'chocolate', description: 'milk chocolate, dark chocolate or white chocolate', url: 'https://url.com' })
-//   .find()  
+  .find()  
 //   .findById(2)  
-  .update(2, { type: 'gummies', description: 'worms, bears, juju\s, novelty shapes, creams, sours and more', url: 'https://url.com'})
+//   .update(2, { type: 'gummies', description: 'worms, bears, juju\s, novelty shapes, creams, sours and more', url: 'https://url.com'})
+//   .delete(2)
   .then(console.log);
 
