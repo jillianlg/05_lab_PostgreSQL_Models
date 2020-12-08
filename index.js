5fc91e2e0cad588732e994179a2a271c2eb289b0
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-07 16:12:21 -0800

    adds UPDATE route for Candy class and returns in console

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Candy = require('./lib/models/Candy');
 Candy
 //   .insert({ type: 'chocolate', description: 'milk chocolate, dark chocolate or white chocolate', url: 'https://url.com' })
 //   .find()  
-  .findById(2)  
+//   .findById(2)  
+  .update(2, { type: 'gummies', description: 'worms, bears, juju\s, novelty shapes, creams, sours and more', url: 'https://url.com'})
   .then(console.log);
 
