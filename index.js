eabab02330a2140dee83386b7f367bf4051c41ad
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-07 16:01:54 -0800

    adds findById route for Candy class and returns in console

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Candy = require('./lib/models/Candy');
 
 Candy
 //   .insert({ type: 'chocolate', description: 'milk chocolate, dark chocolate or white chocolate', url: 'https://url.com' })
-  .find()  
+//   .find()  
+  .findById(2)  
   .then(console.log);
 
